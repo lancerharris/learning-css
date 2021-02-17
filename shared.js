0977515a6a5110a10d603b47d5d8088e04ccc44d
Lance Harris <lancerprogram@gmail.com>
2021-02-17 17:00:04 -0500

    Add sidenav and open and close functionality
    Also moved modal styles (main -> shared)
    to correct .open class inherit issue

--- shared.js
+++ shared.js
@@ -3,19 +3,36 @@ const modal = document.querySelector('.modal');
 const modalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButttons = document.querySelectorAll('.plan button');
 
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
 selectPlanButttons.forEach((button) => {
   button.addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    if (modal) {
+      modal.classList.add('open');
+    }
+    backdrop.classList.add('open');
   });
 });
 
 backdrop.addEventListener('click', () => {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open')
+  }
+  mobileNav.classList.remove('open')
+  backdrop.classList.remove('open')
 });
 
-modalNoButton.addEventListener('click', () => {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
-});
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', () => {
+    modal.classList.remove('open')
+    backdrop.classList.remove('open')
+  });  
+}
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.classList.toggle('open')
+  backdrop.classList.toggle('open')
+})
